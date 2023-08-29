55332aeed742ee8636dae2a29a6f4f0b5fe6123d
StaffanHynge <>
2023-08-29 10:30:45 +0000

    added aryan template and design

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Home
+from .views import Home, aryan
+from . import views
 
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('aryan/', views.aryan, name='aryan'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView): 
     template_name = 'home/home.html'
+
+
+def aryan(request):
+    return render(request, 'home/aryan.html')
